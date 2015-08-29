7551533162c6b89215447a20fe8978bcae4218ad
hb9cwp <rolf.sommerhalder@alumni.ethz.ch>
2015-08-29 12:08:44 +0200

    rearrange output

--- cmd/testBpfSniffer/main.go
+++ cmd/testBpfSniffer/main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
-	"github.com/google/gopacket/bsdbpf"
+	//"github.com/google/gopacket/bsdbpf"
+	"github.com/hb9cwp/gopacket/bsdbpf"
 )
 
 func main() {
@@ -21,20 +22,20 @@ func main() {
 	        if err != nil {
 	         panic(err)
 	        }
-		fmt.Printf("timeStamp %s\n", ci.Timestamp)
+		fmt.Printf("%s ", ci.Timestamp)
 		packet := gopacket.NewPacket(frame, layers.LayerTypeEthernet, gopacket.Default)
-
-		// Get the TCP layer from this packet
-		if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
-			fmt.Println("This is a TCP packet!")
-			// Get actual TCP data from this layer
-			tcp, _ := tcpLayer.(*layers.TCP)
-			fmt.Printf("From src port %d to dst port %d\n", tcp.SrcPort, tcp.DstPort)
-		}
 
 		// Iterate over all layers, printing out each layer type
 		for _, layer := range packet.Layers() {
-			fmt.Println("PACKET LAYER:", layer.LayerType())
+			fmt.Printf(" %v - ", layer.LayerType())
 		}
+
+		// Get the TCP layer from this packet
+		if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+			// Get actual TCP data from this layer
+			tcp, _ := tcpLayer.(*layers.TCP)
+			fmt.Printf("src:%d to dst:%d\n", tcp.SrcPort, tcp.DstPort)
+		}
+		fmt.Println()
 	}
 }
